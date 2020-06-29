e95190c83bbba2e41873ac5c17f67dbbe5f400f5
Josh DeGraw <josh-degraw@pluralsight.com>
2020-06-29 11:48:21 -0600

    Minor updates

--- Src/AutoFixture.Extensions.FSharp/Library.fs
+++ Src/AutoFixture.Extensions.FSharp/Library.fs
@@ -1,38 +1,34 @@
-﻿module AutoFixture.Extensions.FSharp    
+﻿module AutoFixture.Extensions.FSharp
+
 open AutoFixture
 open AutoFixture.Kernel
 
+/// Builds F#-specific specimens
 type FSharpSpecimenBuilder(fixture: IFixture) =
-    interface ISpecimenBuilder with 
+    interface ISpecimenBuilder with
         member this.Create(request, ctx) =
             match request with
             | List t ->
-                [ for _ in 1..fixture.RepeatCount -> ctx.Resolve t ]
+                [ for _ in 1 .. fixture.RepeatCount -> ctx.Resolve t ]
                 |> ReflectiveList.BuildTyped t
             | Union create -> create ctx
             | Unit unitVal -> unitVal
             | _ -> box <| NoSpecimen()
-            
-type FSharpCustomization() =        
-    interface ICustomization with
-        member this.Customize fixture =                
-            FSharpSpecimenBuilder fixture
-            |> fixture.Customizations.Add 
-    
+
 let fsFixture =
     let f = Fixture()
-    let c = FSharpCustomization() :> ICustomization
-    c.Customize f
+
+    f.Customizations.Add(FSharpSpecimenBuilder f)
     f :> IFixture
 
-/// Create a random value. 
-/// In many cases, you don't need to explicitly provide the type argument, which can improve readability 
+/// Create a random value.
+/// In many cases, you don't need to explicitly provide the type argument, which can improve readability
 let inline randVal<'a> () = fsFixture.Create<'a>()
 
 /// Create a sequence of random values.
-/// In many cases, you don't need to explicitly provide the type argument, which can improve readability 
+/// In many cases, you don't need to explicitly provide the type argument, which can improve readability
 let inline randVals<'a> () = fsFixture.CreateMany<'a>()
 
-/// Create a sequence of n random values. 
-/// In many cases, you don't need to explicitly provide the type argument, which can improve readability 
+/// Create a sequence of n random values.
+/// In many cases, you don't need to explicitly provide the type argument, which can improve readability
 let inline randValsN<'a> n = fsFixture.CreateMany<'a>(n)
